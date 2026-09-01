06ecacd828d4178b297395079770c8b8abaa49a5
smali-development <smabn5development@gmail.com>
2026-09-01 17:24:23 +0500

    CardShadcn_updated

--- src/components/CardShadcn.tsx
+++ src/components/CardShadcn.tsx
@@ -3,26 +3,21 @@ import Image from "next/image";
 
 interface CardShadcnProps {
   imagePath: string;
-//   cardName: string;
-//   viewImagesLink: string;
 }
 
-export default function CardShadcn({ imagePath}: CardShadcnProps) {
+export default function CardShadcn({ imagePath }: CardShadcnProps) {
   return (
-    <Card className="w-72">
-   
-      <CardContent>
+    <Card className="w-full">
+      <CardContent className="p-2">
         <Image 
-  src={imagePath} 
-  alt="Image description" 
-  width={150} // Original ya screen ke mutabiq barha size dein
-  height={150} // Sahi ratio ke mutabiq height dein
-  // quality={95} // High quality ke liye 90-95 sab se behtareen hai
-  priority // Yeh lagane se image jaldi load hogi aur saaf dikhegi
-  className="w-full h-auto rounded-md" 
-/>
+          src={imagePath} 
+          alt="Image description" 
+          width={150} 
+          height={150} 
+          priority 
+          className="w-full h-auto rounded-md object-cover" 
+        />
       </CardContent>
-     
     </Card>
   );
 }
