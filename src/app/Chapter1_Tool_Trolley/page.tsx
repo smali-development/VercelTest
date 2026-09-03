8543a7a283df7f058485dffc6de17a2b686694a0
smali-development <smabn5development@gmail.com>
2026-09-03 10:52:17 +0500

    Lowercase_box_Name

--- src/app/Chapter1_Tool_Trolley/page.tsx
+++ src/app/Chapter1_Tool_Trolley/page.tsx
@@ -3,7 +3,13 @@ import CollapsibleSection from "@/components/CollapsibleSection";
 // 1. Import the actual child page components directly
 
 import Box1 from "./box1/page";
-
+import Box2 from "./box2/page";
+import Box3 from "./box3/page";
+import Box4 from "./box4/page";
+import Box5 from "./box5/page";
+import Box6 from "./box6/page";
+import Box7 from "./box7/page";
+import Box8 from "./box8/page";
 
 export default async function ToolTrolleyOverviewPage() {
     return (
@@ -17,7 +23,41 @@ export default async function ToolTrolleyOverviewPage() {
                 <Box1 />
             </CollapsibleSection>
 
+            {/* Render Box 2 page content */}
+            <CollapsibleSection title="Box 2">
+                <Box2 />
+            </CollapsibleSection>
+
+            {/* Render Box 3 page content */}
+            <CollapsibleSection title="Box 3">
+                <Box3 />
+            </CollapsibleSection>
+
             
+             {/* Render Box 4 page content */}
+            <CollapsibleSection title="Box 4">
+                <Box4 />
+            </CollapsibleSection>
+
+             {/* Render Box 5 page content */}
+            <CollapsibleSection title="Box 5">
+                <Box5 />
+            </CollapsibleSection>
+
+             {/* Render Box 6 page content */}
+            <CollapsibleSection title="Box 6">
+                <Box6 />
+            </CollapsibleSection>
+
+             {/* Render Box 7 page content */}
+            <CollapsibleSection title="Box 7">
+                <Box7 />
+            </CollapsibleSection>
+
+             {/* Render Box 8 page content */}
+            <CollapsibleSection title="Box 8">
+                <Box8 />
+            </CollapsibleSection>
         </div>
     );
 }
