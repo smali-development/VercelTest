ebbd5c114059e183eb0c31c9bb52fd0737eeffe7
smali-development <smabn5development@gmail.com>
2026-09-04 11:36:46 +0500

    more

--- src/app/Chapter6_Engine_Base_System/Engine_Proper_System/Valve_Mechanism_Timing/page.tsx
+++ src/app/Chapter6_Engine_Base_System/Engine_Proper_System/Valve_Mechanism_Timing/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Valve_Mechanism_Timing = () => {
-  return (
-    <div>Valve_Mechanism_Timing</div>
-  )
+    return (
+        <div>Valve_Mechanism_Timing</div>
+    )
 }
 
 export default Valve_Mechanism_Timing
